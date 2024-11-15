028d8d6562f3cf027a76abc255ff11052243f434
AlyssaLayla <alyssasasti@gmail.com>
2024-11-16 03:02:24 +0700

    feat: add initial transaksiMyPay with partial functionality and UI setup

---
+++ src/modules/TransaksiMyPayModule/index.tsx
@@ -0,0 +1,12 @@
+// TransaksiMyPayModule/index.tsx
+
+import React from "react";
+import TransaksiMyPay from "./sections/TransaksiMyPaySection";
+
+const TransaksiMyPayModule = () => {
+  return (
+    <TransaksiMyPay userType="Pengguna" userName="John Doe" userBalance={150000} />
+  );
+};
+
+export default TransaksiMyPayModule;
