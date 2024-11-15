2d0f17be66a9ac9ef258f3b5bb42355fd5040ee9
gracekarinn <gracekarina12@gmail.com>
2024-11-15 17:27:42 +0700

    feat: add new interfaces

--- src/modules/DiskonModule/interface.ts
+++ src/modules/DiskonModule/interface.ts
@@ -1,4 +1,4 @@
-export interface Voucher {
+export interface VoucherProps {
   kode: string;
   potongan: string;
   minTransaksi: number;
@@ -7,7 +7,20 @@ export interface Voucher {
   harga: number;
 }
 
-export interface Promo {
+export interface PromoProps {
   kode: string;
   tanggalAkhirBerlaku: string;
+}
+
+export interface SuccessModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  voucherCode: string;
+  expiryDate: string;
+  quota: number;
+}
+
+export interface FailureModalProps {
+  isOpen: boolean;
+  onClose: () => void;
 }
