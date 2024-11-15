f09902afac2db5838c5c8510db1a0ebabdd73f6b
AlyssaLayla <alyssasasti@gmail.com>
2024-11-16 02:53:37 +0700

    feat: add initial MyPay

---
+++ src/modules/MyPayModule/constant.ts
@@ -0,0 +1,19 @@
+import { MyPayProps } from "./interface";
+
+export const MYPAYS: MyPayProps[] = [
+  {
+    nominal: 50000,
+    tanggal: "11 November 2024",
+    kategoriTransaksi: "TopUp MyPay"
+  },
+  {
+    nominal: 100000,
+    tanggal: "2 Agustus 2024",
+    kategoriTransaksi: "Withdrawal"
+  },
+  {
+    nominal: 75000,
+    tanggal: "6 Februari 2024",
+    kategoriTransaksi: "Transfer MyPay"
+  },
+];
