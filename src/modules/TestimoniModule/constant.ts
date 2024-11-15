eea3535ecf7560a6f9742b663e2b28724be18890
gracekarinn <gracekarina12@gmail.com>
2024-11-15 16:12:57 +0700

    feat: add testimonial constants for sample data

---
+++ src/modules/TestimoniModule/constant.ts
@@ -0,0 +1,32 @@
+export const CONTOH_TESTI = [
+  {
+    nama: "Joni",
+    teks: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec purus.",
+    tanggal: "2021-01-01",
+    rating: 5,
+  },
+  {
+    nama: "Doe",
+    teks: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec purus.",
+    tanggal: "2021-01-02",
+    rating: 4,
+  },
+  {
+    nama: "Jono",
+    teks: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec purus.",
+    tanggal: "2021-01-03",
+    rating: 3,
+  },
+  {
+    nama: "Jone",
+    teks: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec purus.",
+    tanggal: "2021-01-04",
+    rating: 2,
+  },
+  {
+    nama: "Jona",
+    teks: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec purus.",
+    tanggal: "2021-01-05",
+    rating: 1,
+  },
+];
