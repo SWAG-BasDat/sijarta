5bf952f204cfb24f834c2fba13002e314afb8927
gracekarinn <gracekarina12@gmail.com>
2024-12-09 23:15:54 +0700

    fix: rename component function to match PascalCase convention

--- src/app/statusPekerjaanJasa/page.tsx
+++ src/app/statusPekerjaanJasa/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import StatusPekerjaanJasaModule from "@/modules/StatusPekerjaanJasaModule";
 
-const page = () => {
+const Page = () => {
   return <StatusPekerjaanJasaModule />;
 };
 
-export default page;
+export default Page;
